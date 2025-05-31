281e052e6f775cf6dfbe9397e8ba1c4f9d203e8d
nataliyarosenberg <nshcherbak@yahoo.com>
2025-05-30 20:48:29 -0700

    three tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,16 @@
-function slugify(title) {
-  const titleSlug = title.toLowerCase().split(' ').join('-');
-  return titleSlug;
+function isEnoughCapacity(products, containerSize) {
+  let sum = 0;
+  const values = Object.values(products);
+  for (const value of values) {
+    sum += value;
+  }
+  return sum <= containerSize;
 }
-console.log(slugify('Arrays for beginners'));
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
+console.log(isEnoughCapacity({ apples: 2, grapes: 4 }, 5));
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
