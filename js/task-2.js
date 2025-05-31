69d6a1ff4f36d76ef01e5381273c0a7648d37c1c
Nataliya Rosenberg <129983835+nataliyarosenberg@users.noreply.github.com>
2025-05-30 19:52:40 -0700

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+function makeArray(firstArray, secondArray, num) {
+  const combo = firstArray.concat(secondArray);
+  return combo.slice(0, num);
+}
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
